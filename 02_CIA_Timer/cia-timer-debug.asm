b3eb85f0400baa4d349cef401860cc2f124329e9
Daniel Molina <dmolina@gmail.com>
2023-03-31 23:26:07 +0200

    .

--- 02_CIA_Timer/cia-timer-debug.asm
+++ 02_CIA_Timer/cia-timer-debug.asm
@@ -1,51 +1,51 @@
-  processor 6502
+ processor 6502
 
-;-------------------------------------------------------                          
 ; Basic UPSTART
-; 2023 SYS start:
-;-------------------------------------------------------                          
-
-  .org $801
+;-------------------------------------------------------
+   .org $801
 basic:
-  .word 0$                ; link to next line     
-  .word 2023              ; line number
-  .byte $9E               ; SYS token
-                          ; Start of SYS digits
-  .if (* + 8) / 10000
-  .byte $30 + (* + 8) / 10000
-  .endif
-  .if (* + 7) / 1000
-  .byte $30 + (* + 7) % 10000 / 1000
-  .endif
-  .if (* + 6) / 100
-  .byte $30 + (* + 6) % 1000 / 100
-  .endif
-  .if (* + 5) / 10
-  .byte $30 + (* + 5) % 100 / 10
-  .endif
-  .byte $30 + (* + 4) % 10
+   .word 0$      ; link to next line
+   .word 1995    ; line number
+   .byte $9E     ; SYS token
+ ; SYS digits
+   .if (* + 8) / 10000
+   .byte $30 + (* + 8) / 10000
+   .endif
+   .if (* + 7) / 1000
+   .byte $30 + (* + 7) % 10000 / 1000
+   .endif
+   .if (* + 6) / 100
+   .byte $30 + (* + 6) % 1000 / 100
+   .endif
+   .if (* + 5) / 10
+   .byte $30 + (* + 5) % 100 / 10
+   .endif
+   .byte $30 + (* + 4) % 10
 0$:
-  .byte 0,0,0   ; end of BASIC program
-;-------------------------------------------------------                          
+   .byte 0,0,0   ; end of BASIC program
 
-;-------------------------------------------------------
-; Entry Point
+; Entry point
 ;-------------------------------------------------------
 
+; Print Title and clear Screen
 start:
-  lda #<greet_msg
-  ldy #>greet_msg
-  jsr $ab1e
-  jsr clrscr
+   lda #<greet_msg
+   ldy #>greet_msg
+   jsr $ab1e
+   jsr clrscr
+
+; Wait until down border to continue 
 restart:
   lda $d011
-  bpl *-3       ; jump if raster bit #8 = 0 Wait for raster > 256
-                ; This is in the border
+  bpl *-3
+
+; $FA is test iteration (0-7)
   lda #$00
   sta $fa
+
+; Jump Point to run next text
 nexttest:
-  ; "clear" CIAs
-  ; CRGs set to all 0. ICR read ack
+  ; "clear" CIAs. CRs to 0. ACK ICR
   lda #$00
   sta $dc0e
   sta $dc0f
@@ -53,15 +53,109 @@ nexttest:
   sta $dd0f
   lda $dc0d
   lda $dd0d
+; SET UP tests
+  ldx $fa             ; Again, $FA is the test index
+; First, we set up registers
+  lda icr,x           ; Load value for ICR data table
+  sta useirc+1        ; STA into USEIRC+1
+  lda cr,x            ; Load value form CR data table
+  sta usecr+1         ; STA into CR+1
+  lda tlow,x
+  sta usetlow+1
+  lda cianr,x         ; CIA NUMBER (dc or dd)
+  sta usecia11+2
+  sta usecia12+2
+  sta usecia13+2
+  sta usecia14+2
+  sta usecr+2
+  sta usetlow+2
+  eor #$01
+  sta usecia21+2
+  sta usecia22+2
+  sta usecia23+2
+  sta usecia24+2
+; Then we set up output locations
+   txa
+   asl
+   tax
+   lda out+1,x
+; $0450,$0518,$0464,$052c,$0608,$06d0,$061c,$06e4   
+   tay
+   lda out,x
+   sta output0+1
+   sty output0+2
+   clc
+   adc #40
+   sta output1+1
+   bcc nr1
+   iny
+nr1:
+   sty output1+2
+   clc
+   adc #40
+   sta output2+1
+   bcc nr2
+   iny
+nr2:
+   sty output2+2
+   clc
+   adc #40
+   sta output3a+1
+   sta output3b+1
+   bcc nr3
+   iny
+nr3:
+   sty output3a+2
+   sty output3b+2
+   clc
+   adc #40
+   sta output4a+1
+   sta output4b+1
+   bcc nr4
+   iny
+nr4:
+   sty output4a+2
+   sty output4b+2
+; setup irq
+  sei
+  lda #$35
+  sta $01
+  lda #<irqhandler
+  sta $fffe
+  lda #>irqhandler
+  sta $ffff
+  lda #<irqhandler
+  sta $fffa
+  lda #>irqhandler
+  sta $fffb
+  lda #$7f
+  sta $dc0d
+  sta $dd0d
+useirc:
+; Here we load icr value from each test
+; $80,$81,$80,$82,$80,$81,$80,$82
+  lda #$80
+  lda #$80
+usecia11:
+  sta $dc0d           ; $dc,$dc,$dc,$dc,$dd,$dd,$dd,$dd  
+  inc $d019
+  lda $dc0d
+  lda $dd0d
+  cli
+
+; do test
+  jsr test
 
   ; next text or restart
   inc $fa
   lda $fa
-  cmp #$08
-  bne nrst
-
-  ; "clear" CIAs
-  ; CRGs set to all 0. ICR read ack
+  cmp #$08    ; IF test is 8, we don't jump
+              ; Just one test for now!
+  bne nrst    ; ELSE jmp to nexttest
+ 
+; "clear" CIAs
+; Here we have completed all test
+; Clear CIAS, and check data
   lda #$00
   sta $dc0e
   sta $dc0f
@@ -69,24 +163,118 @@ nexttest:
   sta $dd0f
   lda $dc0d
   lda $dd0d
-  ; Restore VIC raster interrupt to default
   lda #$1b
   sta $d011
-  jsr checkdata
-  jmp *
+; Still we don't check data, lets analyze the code first
+;   jsr checkdata
+  jmp end
 nrst:
   jmp nexttest
+
+; Halt and Catch Fire
 end:
   jmp end
+; 
+; TEST entry point
 ;-------------------------------------------------------
+test:
+ ; switch off the screen while the test is running, this is
+ ; needed so it can work correctly also on NTSC (where the
+ ; border is much smaller)
+ ; We also wait to the end of the raster in the bottom border
+  lda #$0b
+  sta $d011
+  lda $d011
+  bmi test
+  lda $d011
+  bpl test
+  ldx #$10
+; ACK IRQ
+  lda $dc0d
+  lda $dd0d
+  lda #$00
+  sta $dc04
+  sta $dc06
+  sta $dd04
+  sta $dd06
+  lda #$01
+  sta $dc05
+  sta $dc07
+  sta $dd05
+  sta $dd07
+usecia21:
+  inc $dd04           ; Opposite of $dc,$dc,$dc,$dc,$dd,$dd,$dd,$dd  
+  lda #$11
+usecr:
+  sta $dc0f           ; $0e,$0e,$0f,$0f,$0e,$0e,$0f,$0f
+usecia22:
+  sta $dd0e           ; Opposite of $dc,$dc,$dc,$dc,$dd,$dd,$dd,$dd  
+lp0:
+  lda #$20
+output3a:
+  sta $0478,x         ; Linea 3 del test
+                      ; En esta parte del test, espacios en blanco
+                      ; Se rellena dentro del irqhandler
+output4a:
+  sta $04a0,x         ; Linea 4 del test
+                      ; En esta parte del test, espacios en blanco
+                      ; Se rellena dentro del irqhandler;   ldy #$1a
+  ldy #$1a
+lp1:
+  dey
+  bne lp1
+  lda #$80
+  sec
+usecia23:
+  sbc $dd04           ; Opposite of $dc,$dc,$dc,$dc,$dd,$dd,$dd,$dd  
+  jsr delay
+usetlow:  
+  lda $dc06           ; $dc,$dc,$dc,$dc,$dd,$dd,$dd,$dd  
+  sec
+  sbc #$15
+output0:
+  sta $0400,x         ; Linea 0 del test; es TLOW menos 15?
+usecia12:
+  lda $dc0d           ; $dc,$dc,$dc,$dc,$dd,$dd,$dd,$dd  
+output1:
+  sta $0428,x         ; Linea 1 del test; es ICR
+usecia13:
+  lda $dc0d           ; $dc,$dc,$dc,$dc,$dd,$dd,$dd,$dd  
+output2:
+  sta $0450,x         ; Linea 2 del test; ICT de nuevo
+  dex
+  bne lp0         ; we loop the test x times #$10
+
+; RTS from rest
+  rts
+; 
+; ;-------------------------------------------------------
+  .org $0f00
+irqhandler:
+  
+  pha
+usecia14:
+  lda $dc0d           ; $dc,$dc,$dc,$dc,$dd,$dd,$dd,$dd  
+  pla
+  rti
+
+output3b:
+  sta $0478,x         ; 3 line. ICR within irqhandtler
+usecia24:
+  lda $dd04           ; Opposite of $dc,$dc,$dc,$dc,$dd,$dd,$dd,$dd  
+  clc
+  adc #$10
+output4b:
+  sta $04a0,x         ; 4 line. TLOW within irqhandler
+  pla
+intexit:
+  rti  
 
 
-
-;-------------------------------------------------------
-; Limpia pantalla
+; Clear Screen
 ;-------------------------------------------------------
 clrscr:
-  ldx #$00d
+  ldx #$00
   stx $d020
   stx $d021
 clrlp:
@@ -98,11 +286,10 @@ clrlp:
   inx
   bne clrlp
   rts
+
+
 ;-------------------------------------------------------
-; What it says, accurate 80-A cycle delay
-; org = $1000 so we ensure we do not cross
-; any page boundaries
-;-------------------------------------------------------
+
   .org $1000
 delay:              ;delay 80-accu cycles, 0<=accu<=64
     lsr             
@@ -125,277 +312,6 @@ smod:
   .byte $EA,$EA,$EA,$EA,$EA,$EA,$EA,$EA
     rts             ;6 cycles
 
-;-------------------------------------------------------
-; Variables
-; out: posiciones de salida en pantalla
-; greet_msg: mensaje de bienvenida
-;-------------------------------------------------------
-out:
-  .word $0450,$0518,$0464,$052c,$0608,$06d0,$061c,$06e4
-greet_msg:
-    dc.b 147,"  CIA-TIMERS DMG / REFERENCE: "
-    if DUMP = 0
-    dc.b "OLD"
-    endif
-    if DUMP = 1
-    dc.b "NEW"
-    endif
-    dc.b " CIAS", 13,13,0
-
-;-------------------------------------------------------
-; check and validate results
-;-------------------------------------------------------
-
-checkdata:
-    lda #5
-    sta bordercol
-
-bordercol = * + 1
-    lda #0
-    sta $d020
-
-
-    rts
-
-
-/*
-  ldx $fa
-  lda icr,x
-  sta useirc+1
-  lda cr,x
-  sta usecr+1
-  lda tlow,x
-  sta usetlow+1
-  lda cianr,x
-  sta usecia11+2
-  sta usecia12+2
-  sta usecia13+2
-  sta usecia14+2
-  sta usecr+2
-  sta usetlow+2
-  eor #$01
-  sta usecia21+2
-  sta usecia22+2
-  sta usecia23+2
-  sta usecia24+2
-  txa
-  asl
-  tax
-  lda out+1,x
-  tay
-  lda out,x
-  sta output0+1
-  sty output0+2
-  clc
-  adc #40
-  sta output1+1
-  bcc nr1
-  iny
-nr1:
-  sty output1+2
-  clc
-  adc #40
-  sta output2+1
-  bcc nr2
-  iny
-nr2:
-  sty output2+2
-  clc
-  adc #40
-  sta output3a+1
-  sta output3b+1
-  bcc nr3
-  iny
-nr3:
-  sty output3a+2
-  sty output3b+2
-  clc
-  adc #40
-  sta output4a+1
-  sta output4b+1
-  bcc nr4
-  iny
-nr4:
-  sty output4a+2
-  sty output4b+2
-
-;-------------------------------------------------------
-
-
-
-
-
-
-
-
-;-------------------------------------------------------
-  ; setup irq
-  sei
-  lda #$35
-  sta $01
-  lda #<irqhandler
-  sta $fffe
-  lda #>irqhandler
-  sta $ffff
-  lda #<irqhandler
-  sta $fffa
-  lda #>irqhandler
-  sta $fffb
-  lda #$7f
-  sta $dc0d
-  sta $dd0d
-useirc:
-  lda #$80
-usecia11:
-  sta $dc0d
-  inc $d019
-  lda $dc0d
-  lda $dd0d
-  cli
-; do test
-  jsr test
-
-
-
-
-;-------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-;-------------------------------------------------------
-test:
- ; switch off the screen while the test is running, this is
- ; needed so it can work correctly also on NTSC (where the
- ; border is much smaller)
-  lda #$0b
-  sta $d011
-  lda $d011
-  bmi test
-  lda $d011
-  bpl test
-  ldx #$10
-  lda $dc0d
-  lda $dd0d
-  lda #$00
-  sta $dc04
-  sta $dc06
-  sta $dd04
-  sta $dd06
-  lda #$01
-  sta $dc05
-  sta $dc07
-  sta $dd05
-  sta $dd07
-usecia21:
-  inc $dd04
-  lda #$11
-usecr:
-  sta $dc0f
-usecia22:
-  sta $dd0e
-lp0:
-  lda #$20
-output3a:
-  sta $0478,x
-output4a:
-  sta $04a0,x
-  ldy #$1a
-lp1:
-  dey
-  bne lp1
-  lda #$80
-  sec
-usecia23:
-  sbc $dd04
-  jsr delay
-usetlow:
-  lda $dc06
-  sec
-  sbc #$15
-output0:
-  sta $0400,x
-usecia12:
-  lda $dc0d
-output1:
-  sta $0428,x
-usecia13:
-  lda $dc0d
-output2:
-  sta $0450,x
-  dex
-  bne lp0
-  rts
-
-
-
-
-
-
-
-
-
-
-
-;-------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-;-------------------------------------------------------
-
-  .org $0f00
-irqhandler:
-  pha
-usecia14:
-  lda $dc0d
-output3b:
-  sta $0478,x
-usecia24:
-  lda $dd04
-  clc
-  adc #$10
-output4b:
-  sta $04a0,x
-  pla
-intexit:
-  rti  
-;-------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ;-------------------------------------------------------
 icr:
@@ -406,96 +322,101 @@ tlow:
   .byte $04,$04,$06,$06,$04,$04,$06,$06
 cianr:
   .byte $dc,$dc,$dc,$dc,$dd,$dd,$dd,$dd  
-
-
-
-
-
-
-
-;-------------------------------------------------------
-    ldy #0
-lp
-    ldx #2
-    lda $0428,y
-    cmp data_compare+$000,y
-    bne skp1
-    ldx #5
-skp1
-    txa
-    sta $d828,y
-    cmp #2
-    bne skp1a
-    sta bordercol
-    lda data_compare+$000,y
-    sta $0428,y
-skp1a:
-    ldx #2
-    lda $0528,y
-    cmp data_compare+$100,y
-    bne skp2
-    ldx #5
-skp2
-    txa
-    sta $d928,y
-    cmp #2
-    bne skp2a
-    sta bordercol
-    lda data_compare+$100,y
-    sta $0528,y
-skp2a:
-    ldx #2
-    lda $0628,y
-    cmp data_compare+$200,y
-    bne skp3
-    ldx #5
-skp3
-    txa
-    sta $da28,y
-    cmp #2
-    bne skp3a
-    sta bordercol
-    lda data_compare+$200,y
-    sta $0628,y
-skp3a:
-    iny
-    bne lp
-lpa
-    ldx #2
-    lda $0728,y
-    cmp data_compare+$300,y
-    bne skp4
-    ldx #5
-skp4
-    txa
-    sta $db28,y
-    cmp #2
-    bne skp4a
-    sta bordercol
-    lda data_compare+$300,y
-    sta $0728,y
-skp4a:
-    iny
-    cpy #$c0
-    bne lpa
-
-
-
-    ldx #0 ; success
-    cmp #5
-    beq nofail
-    ldx #$ff ; failure
-nofail:
-    stx $d7ff
-
-data_compare:
+out:
+  .word $0450,$0518,$0464,$052c,$0608,$06d0,$061c,$06e4
+greet_msg:
+    dc.b 147,"  CIA-TIMER TEST / REFERENCE: "
     if DUMP = 0
-    incbin "dump-oldcia.bin"
+    dc.b "OLD"
     endif
     if DUMP = 1
-    incbin "dump-newcia.bin"
+    dc.b "NEW"
     endif
+    dc.b " CIAS", 13,13,0
 
-
-
-*/
+; 
+; ;-------------------------------------------------------
+; checkdata:
+;     lda #5
+;     sta bordercol
+;     ldy #0
+; lp
+;     ldx #2
+;     lda $0428,y
+;     cmp data_compare+$000,y
+;     bne skp1
+;     ldx #5
+; skp1
+;     txa
+;     sta $d828,y
+;     cmp #2
+;     bne skp1a
+;     sta bordercol
+;     lda data_compare+$000,y
+;     sta $0428,y
+; skp1a:
+;     ldx #2
+;     lda $0528,y
+;    cmp data_compare+$100,y
+;    bne skp2
+;    ldx #5
+;skp2
+;    txa
+;    sta $d928,y
+;    cmp #2
+;    bne skp2a
+;    sta bordercol
+;    lda data_compare+$100,y
+;    sta $0528,y
+;skp2a:
+;    ldx #2
+;    lda $0628,y
+;    cmp data_compare+$200,y
+;    bne skp3
+;    ldx #5
+;skp3
+;    txa
+;    sta $da28,y
+;    cmp #2
+;    bne skp3a
+;    sta bordercol
+;    lda data_compare+$200,y
+;    sta $0628,y
+;skp3a:
+;    iny
+;    bne lp
+;lpa
+;    ldx #2
+;    lda $0728,y
+;    cmp data_compare+$300,y
+;    bne skp4
+;    ldx #5
+;skp4
+;    txa
+;    sta $db28,y
+;    cmp #2
+;    bne skp4a
+;    sta bordercol
+;    lda data_compare+$300,y
+;    sta $0728,y
+;skp4a:
+;    iny
+;    cpy #$c0
+;    bne lpa
+;bordercol = * + 1
+;    lda #0
+;    sta $d020
+;    ldx #0 ; success
+;    cmp #5
+;    beq nofail
+;    ldx #$ff ; failure
+;nofail:
+;    stx $d7ff
+;    rts
+;data_compare:
+;    if DUMP = 0
+;    incbin "dump-oldcia.bin"
+;    endif
+;    if DUMP = 1
+;    incbin "dump-newcia.bin"
+;    endif
